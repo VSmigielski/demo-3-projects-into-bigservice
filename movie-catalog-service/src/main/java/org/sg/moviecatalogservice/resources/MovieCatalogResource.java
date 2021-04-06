ba2a9657846cb2293546c4b1a6ad1456c1f3ea9e
Veronica <veronicasmigielski@gmail.com>
2021-04-06 20:07:30 +0200

    Added Rating class and Movie class

--- movie-catalog-service/src/main/java/org/sg/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/org/sg/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,24 +1,51 @@
 package org.sg.moviecatalogservice.resources;
 
 import org.sg.moviecatalogservice.models.CatalogItem;
+import org.sg.moviecatalogservice.models.Movie;
+import org.sg.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(
-                new CatalogItem( "Transformers", "Test", 4)
+        // Steps:
+        // get all rated movieIds
+
+
+        // get all rated movieIds
+        List<Rating> ratingsList = Arrays.asList(
+                new Rating("1234", 3),
+                new Rating("5678", 4)
         );
+
+        return ratingsList.stream()
+                .map(rating -> new CatalogItem("Name", "Desc", rating.getRating()))
+                .map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    return new CatalogItem(movie.getName(), "Description", rating.getRating());
+                })
+                .collect(Collectors.toList());
+
+        // Previous code
+//        return Collections.singletonList(
+//                new CatalogItem( "Transformers", "Test", 4)
+//        );
     }
 
 }
