2f76c8a733a9a49a5875bbb7a9339ebd43d12f67
Veronica <veronicasmigielski@gmail.com>
2021-04-06 22:55:20 +0200

    Updated to show Web client builder

--- movie-catalog-service/src/main/java/org/sg/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/org/sg/moviecatalogservice/resources/MovieCatalogResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -22,11 +23,16 @@ public class MovieCatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
         // Steps:
         // get all rated movieIds
 
+//        Reactive way - asynchronous
+        WebClient.Builder builder = WebClient.builder();
 
         // get all rated movieIds
         List<Rating> ratingsList = Arrays.asList(
@@ -36,7 +42,22 @@ public class MovieCatalogResource {
 
         return ratingsList.stream()
                 .map(rating -> {
-                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    // Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+                    // Web Client Builder Line
+                    // Instance of movie
+                    Movie movie = webClientBuilder.build()
+                            // Http Method Get, Post etc
+                            .get()
+                            // URI with Web URL
+                            .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                            // Retrieval of the info/Go do fetch
+                            .retrieve()
+                            // Whatever body we get back, convert to instance of this class
+                            .bodyToMono(Movie.class)
+                            // blocking execution until Mono container is filled (through call)
+                            .block();
+
                     return new CatalogItem(movie.getName(), "Description", rating.getRating());
                 })
                 .collect(Collectors.toList());
